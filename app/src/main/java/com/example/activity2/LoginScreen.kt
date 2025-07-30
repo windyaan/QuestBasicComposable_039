eabd371688846aeaeb67456e934033db88367103
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-07-30 11:28:58 +0700

    Menambahkan Logo Universitas

--- app/src/main/java/com/example/activity2/LoginScreen.kt
+++ app/src/main/java/com/example/activity2/LoginScreen.kt
@@ -1,16 +1,19 @@
 package com.example.activity2
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -42,5 +45,11 @@ fun LoginScreen() {
         )
 
         Spacer(modifier = Modifier.height(5.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.logo_umy),
+            contentDescription = "Logo Universitas",
+            modifier = Modifier.size(300.dp)
+        )
     }
 }
