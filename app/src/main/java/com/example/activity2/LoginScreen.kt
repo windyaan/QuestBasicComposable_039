b6c639b7ad693f2707aa783121bf2af067671ea8
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-07-30 11:44:09 +0700

    Menambahkan foto profil

--- app/src/main/java/com/example/activity2/LoginScreen.kt
+++ app/src/main/java/com/example/activity2/LoginScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -90,7 +91,14 @@ fun LoginScreen() {
                 .border(4.dp, Color.White, CircleShape),
             tonalElevation = 6.dp
         ){
-
+            Image(
+                painter = painterResource(id = R.drawable.foto),
+                contentDescription = "Foto Profil",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(CircleShape),
+                alignment = Alignment.Center
+            )
         }
     }
 }
