87a88333a19d9ba9d923087be1ea893196d44b7d
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-07-30 11:42:03 +0700

    Komponen pembungkus UI untuk memberikan efek

--- app/src/main/java/com/example/activity2/LoginScreen.kt
+++ app/src/main/java/com/example/activity2/LoginScreen.kt
@@ -2,12 +2,16 @@ package com.example.activity2
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -78,5 +82,15 @@ fun LoginScreen() {
         )
 
         Spacer(modifier = Modifier.height(14.dp))
+
+        Surface(
+            shape = CircleShape,
+            modifier = Modifier
+                .size(350.dp)
+                .border(4.dp, Color.White, CircleShape),
+            tonalElevation = 6.dp
+        ){
+
+        }
     }
 }
