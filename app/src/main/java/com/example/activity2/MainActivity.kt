d2586ae0ec5359768c196d4b72838f3612203996
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-07-30 10:58:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/activity2/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.activity2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.activity2.ui.theme.Activity2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Activity2Theme {
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background) {
+                    LoginScreen()
+                }
+            }
+        }
+    }
+}
