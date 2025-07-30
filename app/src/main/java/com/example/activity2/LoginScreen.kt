19d2af825b5d8cfa989f927ebca36ba1735c4fcf
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-07-30 11:14:03 +0700

    Membuat fungsi dan menetapkan layout column

---
+++ app/src/main/java/com/example/activity2/LoginScreen.kt
@@ -0,0 +1,23 @@
+package com.example.activity2
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoginScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    )
+}
